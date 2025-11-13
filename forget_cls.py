57b94215843cc8057009897e52b9fa36b1df70d7
amrutadeo-22 <96867598+amrutadeo-22@users.noreply.github.com>
2025-11-13 23:21:38 +0530

    Update forget_cls.py

--- forget_cls.py
+++ forget_cls.py
@@ -1,8 +1,11 @@
-all_ds = ['StanfordDogs', 'StanfordCars',  'Caltech101', 'OxfordFlowers'] 
+# all_ds = ['StanfordDogs', 'StanfordCars', 'Caltech101', 'OxfordFlowers', 'CIFAR10Custom']
+all_ds = ['CIFAR10Custom']
+
 
 forget_classes_all = {
-'StanfordDogs': ['Pekinese', 'toy poodle', 'Scotch terrier'],
-'StanfordCars': ['2009 Spyker C8 Coupe','2010 Dodge Ram Pickup 3500 Crew Cab','2011 Ford Ranger SuperCab'],
-'Caltech101':   ['euphonium', 'minaret', 'platypus'],
-'OxfordFlowers':['trumpet creeper', 'gazania', 'tree mallow'],
-                     }
+    'StanfordDogs': ['Pekinese', 'toy poodle', 'Scotch terrier'],
+    'StanfordCars': ['2009 Spyker C8 Coupe','2010 Dodge Ram Pickup 3500 Crew Cab','2011 Ford Ranger SuperCab'],
+    'Caltech101': ['euphonium', 'minaret', 'platypus'],
+    'OxfordFlowers': ['trumpet creeper', 'gazania', 'tree mallow'],
+    'CIFAR10Custom': ['dog' ]  # example classes to forget
+}
